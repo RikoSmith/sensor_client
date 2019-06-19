e8983820cf9a7497b224b7f5c2d3e4d5347f8ed6
Riko <raimbek.riko@gmail.com>
2019-06-19 11:07:08 +0600

    added dual endpoint

--- src/utils/axiosConf.js
+++ src/utils/axiosConf.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost/api"
+var instance = axios.create({
+  baseURL: "http://10.0.40.99/api"
 });
+
+console.log("Testing network");
+axios
+  .get("/sensorData?name=server_room_one")
+  .then(response => {
+    console.log("Chosing local server");
+    //console.log(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+    instance = axios.create({
+      baseURL: "http://allem.localtunnel.me/api"
+    });
+  });
 
 export default instance;
