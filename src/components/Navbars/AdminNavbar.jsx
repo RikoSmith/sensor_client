f9d07ace2e2eaa2cd07c27aed733227067e15871
Riko <raimbek.riko@gmail.com>
2019-06-19 14:28:13 +0600

    fixed bug with  reloading (for local)

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -173,7 +173,10 @@ class AdminNavbar extends React.Component {
                     onClick={e => e.preventDefault()}
                   >
                     <div className="photo">
-                      <img alt="..." src={require("assets/img/anime3.png")} />
+                      <img
+                        alt="..."
+                        src="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwip6brkjPXiAhXF_CoKHd_nBrEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fuser-icon-man-profile-human-avatar-vector-10552509&psig=AOvVaw3YsNMOYtSme6IWkC8HV0NE&ust=1561018384184281"
+                      />
                     </div>
                     <b className="caret d-none d-lg-block d-xl-block" />
                     <p className="d-lg-none">Log out</p>
